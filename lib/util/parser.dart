597576828372eddaaa7346335b9504241bf11d50
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-07-23 20:15:58 -0400

    Add guardian keywords and fix missing quest data handling

--- lib/util/parser.dart
+++ lib/util/parser.dart
@@ -13,64 +13,61 @@ class ThunderstoneYamlCardParser {
   }
 
   Quest _parseQuest(var node) {
+    final empty = List();
     Quest quest = new Quest();
     quest.name = node['Quest'];
     quest.code = node['Code'];
 
-    for (var entry in node['Heroes']) {
+    for (var entry in node['Heroes'] ?? empty) {
       Hero hero = new Hero();
       _parseCard(entry, hero);
       quest.heroes.add(hero);
     }
 
-    for (var entry in node['Items']) {
+    for (var entry in node['Items'] ?? empty) {
       Item item = new Item();
       _parseCard(entry, item);
       quest.items.add(item);
     }
 
-    for (var entry in node['Spells']) {
+    for (var entry in node['Spells'] ?? empty) {
       Spell spell = new Spell();
       _parseCard(entry, spell);
       quest.spells.add(spell);
     }
 
-    for (var entry in node['Weapons']) {
-      Weapon weapon = new Weapon();
-      _parseCard(entry, weapon);
-      quest.weapons.add(weapon);
-    }
-
-    if (node['Allies'] != null) {
-      for (var entry in node['Allies']) {
-        Ally ally = new Ally();
-        _parseCard(entry, ally);
-        quest.allies.add(ally);
+    if (node['Weapons'] != null) {
+      for (var entry in node['Weapons'] ?? empty) {
+        Weapon weapon = new Weapon();
+        _parseCard(entry, weapon);
+        quest.weapons.add(weapon);
       }
     }
 
-    for (var entry in node['Guardians']) {
+    for (var entry in node['Allies'] ?? empty) {
+      Ally ally = new Ally();
+      _parseCard(entry, ally);
+      quest.allies.add(ally);
+    }
+
+    for (var entry in node['Guardians'] ?? empty) {
       Guardian guardian = new Guardian();
       _parseCard(entry, guardian);
       quest.guardians.add(guardian);
     }
 
-    if (node['Dungeon Rooms'] != null) {
-      for (var entry in node['Dungeon Rooms']) {
-        Room room = new Room();
-        _parseCard(entry, room);
-        room.level = entry['Level'];
-        quest.rooms.add(room);
-      }
+    for (var entry in node['Dungeon Rooms'] ?? empty) {
+      Room room = new Room();
+      _parseCard(entry, room);
+      room.level = entry['Level'];
+      quest.rooms.add(room);
     }
 
-    if (node['Monsters'] != null) {
-      for (var entry in node['Monsters']) {
-        Monster monster = new Monster();
-        _parseCard(entry, monster);
-        monster.level = entry['Level'];
-        quest.monsters.add(monster);
-      }
+    for (var entry in node['Monsters'] ?? empty) {
+      Monster monster = new Monster();
+      _parseCard(entry, monster);
+      monster.level = entry['Level'];
+      quest.monsters.add(monster);
     }
 
     return quest;
